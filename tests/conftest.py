b3dcc174551c0a75a841c9be9892b82faed9182e
Ibraheem Tuffaha <ibraheem@milkstraw.ai>
2024-07-16 13:20:02 +0000

    Adapt conftest.py to comply with pre-commit & actions standards

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,4 +1,5 @@
-from typing import Any, Callable, Generator
+from collections.abc import Callable, Generator
+from typing import Any
 
 import pytest
 
@@ -9,36 +10,9 @@ from app.main import app
 
 @pytest.fixture
 def client() -> Generator[TestClient, Any, None]:
-    """
-    Returns a TestClient instance for testing the FastAPI application
-    and clears any dependency overrides after the test.
-
-    Yields:
-        TestClient: A TestClient instance for testing the FastAPI application.
-
-    Example:
-        ```def test_ping_endpoint(client: TestClient) -> None:
-            response = client.get("/ping")
-            assert response.status_code == 200
-        ```
-    """
     yield TestClient(app)
     app.dependency_overrides = {}
-    print("Tests: Application dependency overrides cleared.")
 
 
-def override_dependency(dependency: Callable[..., Any], mocked_response: Any):
-    """
-    Overrides a dependency with a mocked response.
-
-    Parameters:
-        dependency (Callable[..., Any]): The dependency to override.
-        mocked_response (Any): The mocked response to use.
-
-    Returns:
-        None
-
-    Example:
-        `override_dependency(get_data, mocked_data)`
-    """
+def override_dependency(dependency: Callable[..., Any], mocked_response: Any) -> None:
     app.dependency_overrides[dependency] = lambda: mocked_response
