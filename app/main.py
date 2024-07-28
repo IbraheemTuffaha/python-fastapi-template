e86bb5fe84dc486b16ff8df84fdc9fde6523d132
Ibraheem Tuffaha <ibraheem@milkstraw.ai>
2024-07-28 17:35:52 +0000

    Redirect root path to /docs

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
 
 from app.routers import dummy
 
@@ -11,3 +12,8 @@ app.include_router(dummy.router)
 @app.get("/up")
 async def up() -> str:
     return "ok"
+
+
+@app.get("/")
+async def root() -> RedirectResponse:
+    return RedirectResponse(url="/docs")
--- tests/test_main.py
+++ tests/test_main.py
@@ -5,3 +5,9 @@ def test_up_endpoint(client: TestClient) -> None:
     response = client.get("/up")
     assert response.status_code == 200
     assert response.json() == "ok"
+
+
+def test_root_endpoint(client: TestClient) -> None:
+    response = client.get("/", follow_redirects=False)
+    assert response.status_code == 307
+    assert response.headers["location"] == "/docs"
