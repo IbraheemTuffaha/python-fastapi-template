82a454b2d2c5d881f49f0e4c5d34849dee917853
Ibraheem Tuffaha <ibraheem@milkstraw.ai>
2024-08-31 21:40:41 +0000

    Wrap routers in folders & rename files

--- app/main.py
+++ app/main.py
@@ -1,12 +1,12 @@
 from fastapi import FastAPI
 from fastapi.responses import RedirectResponse
 
-from app.v1.routers import router as v1_router
+from app.v1.routers import base_router as v1_router
 
 
 app = FastAPI()
 
-app.include_router(v1_router)
+app.include_router(v1_router.router)
 
 
 @app.get("/up")
