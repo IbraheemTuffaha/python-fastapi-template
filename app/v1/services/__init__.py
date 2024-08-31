19719b8f1f17a42c66d3a9dee1ab3ad5d6441360
Ibraheem Tuffaha <ibraheem@milkstraw.ai>
2024-08-31 21:30:45 +0000

    Separate dependency injection functions in a folder

--- app/v1/services/__init__.py
+++ app/v1/services/__init__.py
@@ -1,4 +1,3 @@
-__all__ = ["get_user_manager", "UserManager"]
+__all__ = ["UserManager"]
 
-from app.v1.services.user.dependency import get_user_manager
 from app.v1.services.user.user_manager import UserManager
