a8c277821f952e503d35f14a259adda2dee4710c
Ibraheem Tuffaha <ibraheem@milkstraw.ai>
2025-02-24 08:25:40 +0000

    Update setup.sh to make devcontainers work

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,3 +1,3 @@
-curl -sSL https://install.python-poetry.org | python3 -
-poetry install
-poetry run pre-commit install
+curl -LsSf https://astral.sh/uv/install.sh | sh
+uv sync --frozen
+uv run pre-commit install
